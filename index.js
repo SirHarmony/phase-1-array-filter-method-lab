315b8b15f2faa6d4b323d386c148046b5a887035
Harmony <harmonymukolwe@gmail.com>
2024-07-02 15:11:48 +0300

    Implements array filter method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+///////////////////////////////////////////////
+function findMatching(arrayOfDriversNames, str) {
+  const newArrayOfDriversNames = arrayOfDriversNames.filter(
+    (driver) => driver.toLowerCase() === str.toLowerCase()
+  );
+
+  return newArrayOfDriversNames;
+}
+
+///////////////////////////////////////////////
+function fuzzyMatch(arrayOfDriversNames, str) {
+  const newArrayOfDriversNames = arrayOfDriversNames.filter(
+    (driver) => driver.slice(0, 2).toLowerCase() === str.toLowerCase()
+  );
+
+  return newArrayOfDriversNames;
+}
+
+///////////////////////////////////////////////
+function matchName(arrayOfDriverObjects, str) {
+  const newArrayOfElements = arrayOfDriverObjects.filter(
+    (element) => element.name === str
+  );
+
+  return newArrayOfElements;
+}
